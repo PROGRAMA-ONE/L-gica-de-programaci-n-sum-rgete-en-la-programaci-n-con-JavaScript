7f39cc83fd908bfaea5cd523a778aa3901ef7ff5
bemm990503@gmail.com <bemm990503@gmail.com>
2025-01-31 21:50:29 -0600

    alert, prompt y let

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-let numeroSecreto = 6;
+/*let numeroSecreto = 6;
 let numeroUsuario = prompt("Me indicas un numero por favor:");
 
 console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto) {
     alert('Acertaste el numero');
-}
+}*/
+
+alert('Hola Mundo');
+prompt("Me indicas un numero por favor:");

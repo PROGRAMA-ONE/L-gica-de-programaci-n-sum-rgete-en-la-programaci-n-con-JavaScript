d1273a6a6b5084f0bb0b699caad4e45df0e237a5
bemm990503@gmail.com <bemm990503@gmail.com>
2025-02-01 17:42:18 -0600

    Desafíos

--- app.js
+++ app.js
@@ -25,10 +25,24 @@ if (eleccion == numeroSecreto) {
     alert('Adivinaste');
 }*/
 
-let contraseniaDelSistema = "contraseniaPrueba!";
+/*let contraseniaDelSistema = "contraseniaPrueba!";
 
 let contrasenia = prompt("Ingrese la contraseña del sistema:");
 
 if (contrasenia == contraseniaDelSistema){
     alert("Acceso al sistema garantizado");
+}*/
+
+alert('¡Bienvenida y bienvenido a nuestro sitio web!');
+let nombre = 'Lua';
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+alert('¡Error! Completa todos los campos');
+let mensajeDeError = '¡Error! Completa todos los campos';
+alert(mensajeDeError);
+nombre = prompt('Ingrese su nombre de usuario:');
+edad = prompt('Ingrese su edad:');
+if (edad >= 18) {
+    alert('¡Puedes obtener tu licencia de conducir!');
 }

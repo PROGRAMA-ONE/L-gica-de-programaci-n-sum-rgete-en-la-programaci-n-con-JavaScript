bcfafdcbe189a9c6ffc131766c75916ee3729702
bemm990503@gmail.com <bemm990503@gmail.com>
2025-02-01 09:34:41 -0600

    console e if

--- app.js
+++ app.js
@@ -7,5 +7,12 @@ if (numeroUsuario == numeroSecreto) {
     alert('Acertaste el numero');
 }*/
 
-alert('Hola Mundo');
-prompt("Me indicas un numero por favor:");
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el numero');
+}
+

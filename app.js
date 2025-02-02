0788f1f2569213f1743ba5a03659d89bc953ad4c
bemm990503@gmail.com <bemm990503@gmail.com>
2025-02-01 19:27:37 -0600

    proyecto del area anterior

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ console.log(numeroUsuario);
 if (numeroUsuario == numeroSecreto) {
     alert('Acertaste el numero');
 }*/
-
 /*let numeroSecreto = 6;
 let numeroUsuario = prompt("Me indicas un numero por favor:");
 
@@ -16,6 +15,45 @@ if (numeroUsuario == numeroSecreto) {
     alert('Acertaste el numero');
 }*/
 
-let mensajeDeBienvenida = 'Bienvenido a la aplicación';
-alert(mensajeDeBienvenida);
+/*let mensajeDeBienvenida = 'Bienvenido a la aplicación';
+alert(mensajeDeBienvenida);*/
+
+/*alert('Bienvenidos al juego del número secreto');
+let eleccion = prompt('Elige un número entre 1 y 10')
+let numeroSecreto = 4;
+if (eleccion == numeroSecreto) {
+    alert('Adivinaste');
+}*/
+
+/*let contraseniaDelSistema = "contraseniaPrueba!";
+
+let contrasenia = prompt("Ingrese la contraseña del sistema:");
+
+if (contrasenia == contraseniaDelSistema){
+    alert("Acceso al sistema garantizado");
+}*/
+
+/*alert('¡Bienvenida y bienvenido a nuestro sitio web!');
+let nombre = 'Lua';
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+alert('¡Error! Completa todos los campos');
+let mensajeDeError = '¡Error! Completa todos los campos';
+alert(mensajeDeError);
+nombre = prompt('Digite su nombre:');
+edad = prompt('Digite su edad:');
+if (edad >= 18) {
+    alert('¡Puedes obtener tu licencia de conducir!');
+}*/
+
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el numero');
+}
+
 

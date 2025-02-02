1d1bcab71a2b66d2b01835b5417914a6457520c4
bemm990503@gmail.com <bemm990503@gmail.com>
2025-02-01 19:49:40 -0600

    comentarios y else

--- app.js
+++ app.js
@@ -25,10 +25,42 @@ if (eleccion == numeroSecreto) {
     alert('Adivinaste');
 }*/
 
-let contraseniaDelSistema = "contraseniaPrueba!";
+/*let contraseniaDelSistema = "contraseniaPrueba!";
 
 let contrasenia = prompt("Ingrese la contraseña del sistema:");
 
 if (contrasenia == contraseniaDelSistema){
     alert("Acceso al sistema garantizado");
+}*/
+
+/*alert('¡Bienvenida y bienvenido a nuestro sitio web!');
+let nombre = 'Lua';
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+alert('¡Error! Completa todos los campos');
+let mensajeDeError = '¡Error! Completa todos los campos';
+alert(mensajeDeError);
+nombre = prompt('Digite su nombre:');
+edad = prompt('Digite su edad:');
+if (edad >= 18) {
+    alert('¡Puedes obtener tu licencia de conducir!');
+}*/
+
+//Variables
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+
+console.log(numeroUsuario);
+
+/*
+Este código realiza 
+la comparación.
+*/
+if (numeroUsuario == numeroSecreto) {
+    //Acertamos, fue verdadera la condición.
+    alert('Acertaste el numero');
+}else{
+    //La condición no se cumplió.
+    alert('Lo siento, no acertaste el número');
 }

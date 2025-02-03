b2467e3dfdb56e402da39c95cea7822ea843d4b8
bemm990503@gmail.com <bemm990503@gmail.com>
2025-02-02 18:16:21 -0600

    contador--

--- app.js
+++ app.js
@@ -182,9 +182,26 @@ la comparación.
   }
 }*/
 
+/*
 let contador = 1;
 
 while (contador < 4) {
   console.log('Ejecutando la iteración ' + contador);
   contador = contador + 1;
 }
+*/
+
+let cantidadNumeros = prompt('Ingrese la cantidad de números para el cálculo del promedio:');
+let suma = 0;
+let contador = cantidadNumeros;
+
+while(contador > 0){
+    let numero = parseInt(prompt('Ingrese el numero:'));
+    contador--;
+    suma += numero;
+    alert(`Numero ${numero}, Suma ${suma}, Contador ${contador}`);
+}
+
+let promedio = suma / cantidadNumeros;
+
+console.log(promedio);

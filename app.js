c76a629ea40c0d0937512b1cd5eba72e45397747
bemm990503@gmail.com <bemm990503@gmail.com>
2025-02-03 20:34:11 -0600

    break

--- app.js
+++ app.js
@@ -229,11 +229,45 @@ console.log(numeroMaximo);
 numeroMaximo--;
 }
 */
-
+/*
 let numeroMaximo = prompt("Ingresa un número para la cuenta progresiva:");;
 let contador = 0;
 
 while (contador <= numeroMaximo) {
 console.log(contador);
 contador++
+}
+*/
+
+let numeroSecreto = 5;
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez'
+
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+  console.log(numeroUsuario);
+/*
+Este código realiza 
+la comparación.
+*/
+  if (numeroUsuario == numeroSecreto) {
+    //Acertamos, fue verdadera la condición.
+    alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert("El número secreto es menor");
+    } else {
+      alert("El número secreto es mayor");
+    }
+    //incrementamos el contador cuando no acierta
+    intentos = intentos + 1;
+    palabraVeces = 'veces';
+    if(intentos>3){
+      alert('Llegaste al máximo de intentos');
+      break;
+    }
+    //La condición no se cumplió.
+    //alert('Lo siento, no acertaste el número');
+  }
 }

559bb76d0875cbe2d61c36cfb162e3493c6b2e2b
bemm990503@gmail.com <bemm990503@gmail.com>
2025-02-04 21:30:00 -0600

    mathrandom

--- app.js
+++ app.js
@@ -339,7 +339,7 @@ la comparación.
   }
 }
   */
-
+/*
 let cantidadPersonas = prompt("Ingrese la cantidad de personas:");
 console.log(cantidadPersonas);
 
@@ -352,5 +352,48 @@ if(cantidadPersonas == 1){
     palabraPersona = "personas"
 }
 */
+//console.log(palabraPersona);
 
-console.log(palabraPersona);
+
+
+let numeroSecreto = Math.floor(Math.random() * 10)+1;
+
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = 3;
+
+console.log(typeof(numeroSecreto));
+
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
+  console.log(numeroUsuario);
+
+
+/*
+Este código realiza 
+la comparación.
+*/
+
+  if (numeroUsuario == numeroSecreto) {
+    //Acertamos, fue verdadera la condición.
+    alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert("El número secreto es menor");
+    } else {
+      alert("El número secreto es mayor");
+    }
+    //incrementamos el contador cuando no acierta
+    //intentos = intentos + 1;
+    //intentos += 1;
+    intentos++;
+    //palabraVeces = 'veces';
+    if(intentos>maximosIntentos){
+      alert(`legaste al máximo de ${maximosIntentos} intentos`);
+      break;
+    }
+    //La condición no se cumplió.
+    //alert('Lo siento, no acertaste el número');
+  }
+}
